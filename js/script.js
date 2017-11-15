ca824a88f18c1d7d8e1c6c66e7379894fba9bf11
SANG HAN <rv498@hotmail.com>
2017-11-15 07:26:24 -0800

    commit 2

--- js/script.js
+++ js/script.js
@@ -104,7 +104,8 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
                 // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
                 // variable's name implies it expects.
-                // var chosenCategoryShortName = 
+                 var chosenCategoryShortName = chooseRandomCategory(categories);
+                 
 
 
                 // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -118,7 +119,7 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
                 // Hint: you need to surround the chosen category short name with something before inserting
                 // it into the home html snippet.
                 //
-                // var homeHtmlToInsertIntoMainPage = ....
+                // var homeHtmlToInsertIntoMainPage = 
 
 
                 // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
